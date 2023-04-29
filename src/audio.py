f766d65058f7fd8bd83c5780bdd1fab55e0ce0e5
john.shen <john.shen@bioclinica.com>
2023-04-29 12:37:21 +0800

    debug audio

--- src/audio.py
+++ src/audio.py
@@ -2,6 +2,9 @@ import os
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from googleapiclient.errors import HttpError
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 import structlog
 from gtts import gTTS
 
@@ -12,11 +15,33 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 api_service_name = "drive"
 api_version = "v3"
 
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
 
 def upload_to_gdrive(file_path):
+    """Shows basic usage of the Drive v3 API.
+        Prints the names and ids of the first 10 files the user has access to.
+        """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
     try:
         # create drive api client
-        service = build(api_service_name, api_version, developerKey=GOOGLE_API_KEY)
+        service = build(api_service_name, api_version, credentials=creds)
         file_metadata = {'name': file_path}
         logger.info(f"About to upload file {file_path}")
         media = MediaFileUpload(file_path)
