f5a4fb8e19af1c1d605195ba09a0bc2ad2b3f3fe
john.shen <john.shen@bioclinica.com>
2023-04-29 12:40:30 +0800

    debug audio

--- src/audio.py
+++ src/audio.py
@@ -34,7 +34,7 @@ def upload_to_gdrive(file_path):
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'src/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
